595f02e48c5230fb06b4b50594e9e2b671efb7f5
Zsolt Mandak <zsolt.mandak@attrecto.com>
2019-02-24 22:23:27 +0100

    Tuples example

--- src/com/attrecto/kotlinkurzus/Lesson3/Tuples.kt
+++ src/com/attrecto/kotlinkurzus/Lesson3/Tuples.kt
@@ -1,7 +1,14 @@
 package com.attrecto.kotlinkurzus.Lesson3
 
-fun capitalAndPopulation(country: String): Pair<String, Long> {
-    return Pair("Madrid", 23000000)
+fun capitalAndPopulation(capital: String): Pair<String, Long> {
+    val capAndPopList = listOf<Pair<String, Long>>(
+        Pair("Madrid", 23000),
+        Pair("Budapest", 2000000)
+    )
+
+    val result = capAndPopList.firstOrNull { it.first == capital }
+
+    return result?: Pair("",0L)
 }
 
 fun countryInformation(country: String): Triple<String, String, Long> {
@@ -11,12 +18,13 @@ fun countryInformation(country: String): Triple<String, String, Long> {
 fun main() {
 
     val result = capitalAndPopulation("Madrid")
-    println(result.first)
-    println(result.second)
+    println(result?.first)
+    println(result?.second)
 
     val countryInfo = countryInformation("Madrid")
     println(countryInfo.takeIf { countryInfo.second.isNotEmpty() })
 
-    val(countryName, population) = capitalAndPopulation("Madrid")
+    val (countryName, population) = capitalAndPopulation(capital = "ASDAD")
+    if(countryName.isNotEmpty()) println(countryName) else println("No country like this")
     println("Name of the country: $countryName - population: $population")
 }
