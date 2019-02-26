2796e29986eed89545578f9f65d77e7c9f174fc7
Zsolt Mandak <zsolt.mandak@attrecto.com>
2019-02-26 12:30:10 +0100

    Lesson4 Higher Order funs

--- src/com/attrecto/kotlinkurzus/Lesson4/GettingFuncional.kt
+++ src/com/attrecto/kotlinkurzus/Lesson4/GettingFuncional.kt
@@ -7,9 +7,38 @@ fun operation(x: Int, y: Int, op: (Int, Int) -> Int): Int {
 
 fun sum(x: Int, y: Int) = x + y
 
+fun unaryOperation(x: Int, op: (Int) -> Int) {}
+
+fun transaction(db: Database, code: () -> Unit) {
+    try {
+        code()
+    } finally {
+        db.commit()
+    }
+}
+
+class Database {
+    fun commit() {
+
+    }
+}
+
 fun main() {
     println(operation(1, 2, ::sum))
 
     //With lambda expression
     println(operation(1, 2) { x, y -> x + y })
+
+    val sumLambda: (Int, Int) -> Int = { x, y -> x + y }
+    operation(1, 2, sumLambda)
+
+    unaryOperation(2, { x -> x * x })
+    unaryOperation(3, { it * it })
+
+    val db = Database()
+    transaction(db) {
+        //interaction here
+    }
+
+
 }
